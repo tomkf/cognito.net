0828964edb25d7afd8bab9d2fb9fdd647892cd67
tomkf <thomaskeithferris@gmail.com>
2020-01-21 12:27:49 -0800

    config update

--- cognito.net/Startup.cs
+++ cognito.net/Startup.cs
@@ -30,8 +30,7 @@ namespace cognito.net
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddCognitoIdentity();
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
